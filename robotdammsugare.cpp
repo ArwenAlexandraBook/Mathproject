0c7659dd740e6daa53aefa4cdb3a2c2afb568cb0
Arwen Alexandra <90157855+ArwenAlexandraBook@users.noreply.github.com>
2023-03-21 09:21:51 +0100

    Add files via upload
    Inte färdig än och vet att jag passerat deadline men work in progress!

---
+++ robotdammsugare.cpp
@@ -0,0 +1,38 @@
+// title: Mathproject
+// author: Arwen Alexandra Book
+// date: 2023-03-11
+//Description: Calculates distance to object from measured time.
+
+# include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main ()
+{
+const double SPEED_OF_LIGHT = 299792458; /* Speed of light*/
+double time_to_distance;
+double distance;
+float time;
+double coordinates
+
+
+/*function that converts time to distance*/
+time_to_distance = (time * SPEED_OF_LIGHT) /2;
+
+//Short test. Assume 20 ns (nanoseconds) = 2.0 x 10^-8 s
+laserTimeMeasurement = 0.0000002
+cout << "The measured distance is: " << distance << "meters"<<endl;
+
+
+// Coordinates//
+int x = 0.0;
+int y = 0.0;
+theta = (theta-90) * Math.PI/180;  //radian is 45 degrees
+
+// Formula for distination point
+xx = x + (d * cos(alpha))
+yy = y + (d * sin(alpha))
+
+return 0;
+}
